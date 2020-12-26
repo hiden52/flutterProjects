5df87f3efa1617000d82671629a034e88fa95308
정상훈 <20150599@office.bufs.ac.kr>
2020-12-26 11:29:28 +0000

    basic flutter app

--- lib/hello/lib/main.dart
+++ lib/hello/lib/main.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
 
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Center(
+//         child: Text('Hello, World!'),
+//       ),
+//     ),
+//   );
+// }
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Text('Hello!'),
+//     );
+//   }
+// }
+
 void main() {
-  runApp(
-    MaterialApp(
-      home: Center(
-        child: Text('Hello, World!'),
-      ),
-    ),
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext cxt) {
+    return MaterialApp(
+      home: Text('Hello'),
+    );
+  }
 }
